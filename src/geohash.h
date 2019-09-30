0c0c268c6358ebfcff418a1800a03c87facc40bd
Michael Chirico <michael.chirico@grabtaxi.com>
2019-09-30 13:47:27 +0800

    more concise mapping

--- src/geohash.h
+++ src/geohash.h
@@ -8,7 +8,7 @@ SEXP gh_decode(SEXP gh, SEXP include_delta_arg, SEXP coord_loc);
 SEXP gh_neighbors(SEXP gh, SEXP self_arg);
 
 // ** FOR ENCODING **
-// geohash orientation rotates from Z order (odd) to N order (even)
+// NB geohash orientation rotates from Z order (odd) to N order (even)
 static const char map[4][9] = {
   "0145hjnp",
   "2367kmqr",
@@ -22,43 +22,31 @@ static const uint64_t mult8 = 3ULL << 52;
 
 // ** FOR DECODING **
 // offsets relative to LHS of the _longer_ side of the 8x4 (or 4x8) geohash rectangle
+// see cat(rawToChar(as.raw(13:127)))
+// and/or charToRaw('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+#define _ INT_MIN // for conciseness
 static const int offset8[128] = {
-  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,
-  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,
-  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,
-  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,
-  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,
-  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,
-        0,        1,        0,        1,        2,        3,       2,         3,
-        0,        1,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,
-  INT_MIN,  INT_MIN,        0,        1,        2,        3,       2,         3,
-        4,  INT_MIN,        5,        4,  INT_MIN,        5,       6,   INT_MIN,
-        7,        6,        7,        4,        5,        4,       5,         6,
-        7,        6,        7,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,
-  INT_MIN,  INT_MIN,        0,        1,        2,        3,       2,         3,
-        4,  INT_MIN,        5,        4,  INT_MIN,        5,       6,   INT_MIN,
-        7,        6,        7,        4,        5,        4,       5,         6,
-        7,        6,        7,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,
+  _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, // 00-0f
+  _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, // 10-1f
+  _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, // 20-2f
+  0, 1, 0, 1, 2, 3, 2, 3, 0, 1, _, _, _, _, _, _, // 30-3f : 0-9
+  _, _, 0, 1, 2, 3, 2, 3, 4, _, 5, 4, _, 5, 6, _, // 40-4f : B-HJKMN
+  7, 6, 7, 4, 5, 4, 5, 6, 7, 6, 7, _, _, _, _, _, // 50-5f : P-Z
+  _, _, 0, 1, 2, 3, 2, 3, 4, _, 5, 4, _, 5, 6, _, // 60-6f : a-hjkmn
+  7, 6, 7, 4, 5, 4, 5, 6, 7, 6, 7, _, _, _, _, _, // 70-7f : p-z
 };
 // offsets relative to LHS of the _shorter_ side of the 8x4 (or 4x8) geohash rectangle
-static const int offset4 [128] = {
-  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,
-  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,
-  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,
-  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,
-  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,
-  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,
-        0,        0,        1,        1,        0,        0,       1,         1,
-        2,        2,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,
-  INT_MIN,  INT_MIN,        3,        3,        2,        2,       3,         3,
-        0,  INT_MIN,        0,        1,  INT_MIN,        1,       0,   INT_MIN,
-        0,        1,        1,        2,        2,        3,       3,         2,
-        2,        3,        3,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,
-  INT_MIN,  INT_MIN,        3,        3,        2,        2,       3,         3,
-        0,  INT_MIN,        0,        1,  INT_MIN,        1,       0,   INT_MIN,
-        0,        1,        1,        2,        2,        3,       3,         2,
-        2,        3,        3,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,  INT_MIN,
+static const int offset4[128] = {
+  _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _,
+  _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _,
+  _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _,
+  0, 0, 1, 1, 0, 0, 1, 1, 2, 2, _, _, _, _, _, _,
+  _, _, 3, 3, 2, 2, 3, 3, 0, _, 0, 1, _, 1, 0, _,
+  0, 1, 1, 2, 2, 3, 3, 2, 2, 3, 3, _, _, _, _, _,
+  _, _, 3, 3, 2, 2, 3, 3, 0, _, 0, 1, _, 1, 0, _,
+  0, 1, 1, 2, 2, 3, 3, 2, 2, 3, 3, _, _, _, _, _,
 };
+#undef _
 
 static const int centeroffx[9] = {0, 1, 2, 0, 1, 2, 0, 1, 2};
 static const int centeroffy[9] = {0, 0, 0, 1, 1, 1, 2, 2, 2};
