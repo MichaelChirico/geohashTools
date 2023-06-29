13352474c83b4d9e7268346ebd56e2449fb8f8dd
Garnet VanWeerden <garnet_vanweerden@cooperators.ca>
2023-06-29 15:06:35 -0400

    Replaced sum() > 0 with use of any() instead. Also got rid of whitespace.

--- R/gh_fill.R
+++ R/gh_fill.R
@@ -4,25 +4,17 @@
 #' @param precision Positive integer scalar controlling the 'zoom level' – how many characters should be used in the output.
 #' @return Character vector of geohashes corresponding to the input.
 #' @export
-
-
-
 gh_fill <- function(geohashes, precision) {
-  if (uniqueN(nchar(geohashes)) > 1) {
+  if (length(unique(nchar(geohashes))) > 1) {
     stop("Input Geohashes must all have the same precision level.")
   }
-  if (sum(grepl("['ailo]", geohashes)) > 0) {
+  if (any(grepl("['ailo]", geohashes))) {
     stop("Invalid Geohash; Valid characters: [0123456789bcdefghjkmnpqrstuvwxyz]")
   }
-
   new_levels <- precision - nchar(geohashes[1])
-
   base32 <-
     unlist(strsplit("0123456789bcdefghjkmnpqrstuvwxyz", split = ""))
-
   grid <-
     do.call(data.table::CJ, append(list(geohashes), replicate(new_levels, base32, FALSE)))
-
   do.call(paste0, grid)
-
 }
