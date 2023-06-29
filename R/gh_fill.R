07ec5b7d3f68e7d5b714ae87747ef626c60e8305
Garnet VanWeerden <garnet_vanweerden@cooperators.ca>
2023-06-29 15:52:21 -0400

    Updated case sensitivity to reject upper case AILO also.

--- R/gh_fill.R
+++ R/gh_fill.R
@@ -8,8 +8,8 @@ gh_fill <- function(geohashes, precision) {
   if (length(unique(nchar(geohashes))) > 1) {
     stop("Input Geohashes must all have the same precision level.")
   }
-  if (any(grepl("['ailo]", geohashes))) {
-    stop("Invalid Geohash; Valid characters: [0123456789bcdefghjkmnpqrstuvwxyz]")
+  if (any(grepl("['ailoAILO]", geohashes))) {
+    stop("Invalid Geohash; Valid characters: [0123456789bcdefghjkmnpqrstuvwxyz](any case)")
   }
   new_levels <- precision - nchar(geohashes[1])
   base32 <-
