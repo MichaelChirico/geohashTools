208caaa6d6fb65f28f3d6f753cbe5c3130511dc9
Michael Chirico <chiricom@google.com>
2026-06-09 23:29:26 -0700

    More fully integrate package with roxygen2 (#51)
    * manage namespace with roxygen
    
    * man pages migrated to R/
    
    * narrower lines

--- R/onLoad.R
+++ R/onLoad.R
@@ -3,6 +3,7 @@
 .global = new.env(parent = emptyenv())
 setPackageName('geohashTools', .global)
 
+#' @useDynLib geohashTools, .registration = TRUE
 .onLoad <- function(libname, pkgname) {
   # 64-bit integer can fit lat&lon components in 5 bits per 2 units precision
   .global$GH_MAX_PRECISION = 25L
--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,28 @@
-# get cell half-widths for a given geohash precision
-
+#' Geohash utilities
+#'
+#' Various common functions that arise when working often with geohashes
+#'
+#' @param precision `integer` precision level desired.
+#'
+#' @note
+#' *Caveat coder*: not much is done in the way of consistency checking since this is a convenience function.
+#' So e.g. real-valued "precision"s will give results.
+#'
+#' @return
+#' Length-2 `numeric` vector; the first element is the *latitude* (y-coordinate) half-width at the input
+#' `precision`, the second element is the *longitude* (x-coordinate).
+#'
+#' @references
+#' <http://geohash.org/> ( Gustavo Niemeyer's original geohash service )
+#'
+#' @author
+#' Michael Chirico
+#'
+#' @examples
+#' gh_delta(6)
+#'
+#' @name utils
+#' @export
 gh_delta = function(precision) {
   if (length(precision) > 1L) stop('One precision at a time, please.')
   45.0/2.0^((5.0*precision + c(-1.0, 1.0) * (precision %% 2.0))/2.0 - 1:2)
